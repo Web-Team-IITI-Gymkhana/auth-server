a135be61b9ba5c0de71760aff3813464e56ed2d1
Tanwish2003 <106272246+Tanwish2003@users.noreply.github.com>
2023-05-13 11:43:58 +0530

    removing deploy errors 2 (#4)

---
+++ src/db/models/Profile.ts
@@ -0,0 +1,34 @@
+import { Column, DataType, Model, Table, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { UserModel } from './user';
+import sequelize from 'sequelize';
+import { randomUUID } from 'crypto';
+
+@Table({
+  tableName: 'profile',
+})
+export class ProfileModel extends Model {
+  @Column({
+    primaryKey: true,
+    allowNull: false,
+    type: sequelize.UUID,
+    defaultValue: sequelize.UUIDV4,
+  })
+  id: typeof randomUUID;
+  @Column({
+    type: DataType.STRING(255),
+  })
+  name: string;
+
+  @Column({ type: DataType.STRING })
+  imageUrl: Record<string, any>;
+
+  @Column({ type: DataType.JSON })
+  metadata: Record<string, any>;
+
+  @ForeignKey(() => UserModel)
+  @Column(sequelize.UUID)
+  userId: string;
+
+  @BelongsTo(() => UserModel, 'id')
+  user: UserModel;
+}
